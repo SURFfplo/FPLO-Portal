24f231a5f5d930a8bd8937995b780d07217af42a
Frank Niesten <f.niesten@fontys.nl>
2019-04-18 14:44:18 +0200

    Basic version
    Basic Version of the portal with SAML authentication and a simple tile.

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    /**
+	 * @Route("/", name="homepage")
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function index()
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
